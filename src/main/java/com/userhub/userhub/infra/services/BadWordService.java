e4046aeb24e91afa402023d57e4fa57ea2d20b25
Pedro Costa <pedroh.costa083@gmail.com>
2024-12-27 14:03:37 -0300

    feat: implement BadWordService to load and retrieve bad words from a JSON file

---
+++ src/main/java/com/userhub/userhub/infra/services/BadWordService.java
@@ -0,0 +1,38 @@
+// filepath: /c:/Users/pedro.barros/Desktop/Estudos/userhub/src/main/java/com/userhub/userhub/infra/services/BadWordService.java
+package com.userhub.userhub.infra.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BadWordService {
+
+    private static final Logger logger = LoggerFactory.getLogger(BadWordService.class);
+
+    private final List<String> badWords;
+    private final String filePath = "C:\\Users\\pedro.barros\\Desktop\\Estudos\\userhub\\src\\main\\resources\\config\\badwords.json";
+
+    public BadWordService() throws IOException {
+        this.badWords = loadBadWords(filePath);
+    }
+
+    private List<String> loadBadWords(String filePath) throws IOException {
+        String content = Files.readString(Path.of(filePath));
+        logger.debug("BadWordService - File Content: {}", content); // Log do conteúdo do arquivo
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, List<String>> map = objectMapper.readValue(content, new TypeReference<Map<String, List<String>>>() {
+        });
+        logger.debug("BadWordService - Parsed Map: {}", map); // Log do mapa resultante
+        return map.get("badwords");
+    }
+
+    public List<String> getBadWords() {
+        return badWords;
+    }
+}
