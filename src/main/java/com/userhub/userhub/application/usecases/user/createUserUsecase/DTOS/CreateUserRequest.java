07bcabc920e9fe69c401671b7d1a304d9e8d43ec
Pedro Costa <pedroh.costa083@gmail.com>
2024-12-27 14:02:16 -0300

    feat: add roles field to CreateUserRequest and update CreateUserResponse to use String for email

--- src/main/java/com/userhub/userhub/application/usecases/user/createUserUsecase/DTOS/CreateUserRequest.java
+++ src/main/java/com/userhub/userhub/application/usecases/user/createUserUsecase/DTOS/CreateUserRequest.java
@@ -8,13 +8,16 @@ public class CreateUserRequest {
     private String email;
     private LocalDate birthday;
     private String name;
+    private String[] roles;
 
-    public CreateUserRequest(String name, String username, String email, String password, LocalDate birthday) {
+    public CreateUserRequest(String name, String username, String email, String password, LocalDate birthday,
+            String[] roles) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.birthday = birthday;
         this.name = name;
+        this.roles = roles;
     }
 
     public String getUsername() {
@@ -36,5 +39,11 @@ public class CreateUserRequest {
     public String getName() {
         return name;
     }
+
+    public String[] getRoles() {
+        return roles;
+    }
+
+    
 
 }
--- src/main/java/com/userhub/userhub/application/usecases/user/createUserUsecase/DTOS/CreateUserResponse.java
+++ src/main/java/com/userhub/userhub/application/usecases/user/createUserUsecase/DTOS/CreateUserResponse.java
@@ -2,17 +2,15 @@ package com.userhub.userhub.application.usecases.user.createUserUsecase.DTOS;
 
 import java.util.UUID;
 
-import com.userhub.userhub.domain.objetcValues.Email;
-
 public class CreateUserResponse {
 
     private UUID id;
     private String name;
-    private Email email;
+    private String email;
     private String username;
     private String message;
 
-    public CreateUserResponse(UUID id, String name, Email email, String username, String message) {
+    public CreateUserResponse(UUID id, String name, String email, String username, String message) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -28,7 +26,7 @@ public class CreateUserResponse {
         return name;
     }
 
-    public Email getEmail() {
+    public String getEmail() {
         return email;
     }
 
